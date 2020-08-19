995bc6638aaa77eb05d8f91b53aa30e5f2781eec
Joren Schelkens <52953553+JorenSchelkens@users.noreply.github.com>
2020-08-19 15:30:33 +0200

    Added IDomainValidation

---
+++ Core/Database/Domain/Core/Derivations/Default/DomainValidation.cs
@@ -0,0 +1,18 @@
+// <copyright file="IDomainChangeSet.cs" company="Allors bvba">
+// Copyright (c) Allors bvba. All rights reserved.
+// Licensed under the LGPL license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace Allors.Domain.Derivations.Default
+{
+    using Allors.Domain.Derivations.Errors;
+
+    public class DomainValidation : IDomainValidation
+    {
+        private Validation Validation { get; }
+
+        public DomainValidation(Validation validation) => this.Validation = validation;
+
+        public void AddError(string error) => this.Validation.AddError(new DerivationErrorGeneric(this.Validation, relation: null, "End date should be after start date"));
+    }
+}
