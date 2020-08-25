30b866339fed8ea62bc1a7af78db273987f36c67
Sharif Jubayer Arefin <jubayerarefin@users.noreply.github.com>
2020-08-25 16:50:38 +0600

    Update Demos Complete

--- build/Paths.Security.cs
+++ build/Paths.Security.cs
@@ -2,7 +2,7 @@ using Nuke.Common.IO;
 
 public partial class Paths
 {
-    public AbsolutePath Security => Root / "Demos/security";
+    public AbsolutePath Security => Root / "Demos/Security";
     public AbsolutePath SecurityRepositoryDomainRepository => Security / "Repository/Domain/Repository.csproj";
     public AbsolutePath SecurityDatabaseMetaGenerated => Security / "Database/Meta/generated";
     public AbsolutePath SecurityDatabaseGenerate => Security / "Database/Generate/Generate.csproj";
