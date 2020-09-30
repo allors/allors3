3540650f3a515908f3540901480875529a43e742
martienvanknippenberg <martien@dipu.com>
2020-09-30 12:59:26 +0200

    sync from allors2

--- Base/Database/Domain/Base/WorkEffort/WorkEffortType.cs
+++ Base/Database/Domain/Base/WorkEffort/WorkEffortType.cs
@@ -9,27 +9,19 @@ namespace Allors.Domain
 
     public partial class WorkEffortType
     {
-        private bool IsDeletable =>
-            !this.ExistMaintenanceAgreementsWhereWorkEffortType;
-
         public void BaseOnDerive(ObjectOnDerive method)
         {
             var derivation = method.Derivation;
 
             derivation.Validation.AssertExists(this, M.WorkEffortType.Description);
-        }
 
-        public void BaseOnPostDerive(ObjectOnPostDerive method)
-        {
-            var deletePermission = new Permissions(this.Strategy.Session).Get(this.Meta.ObjectType, this.Meta.Delete, Operations.Execute);
-            if (this.IsDeletable)
-            {
-                this.RemoveDeniedPermission(deletePermission);
-            }
-            else
-            {
-                this.AddDeniedPermission(deletePermission);
-            }
+            this.CurrentWorkEffortPartStandards = this.WorkEffortPartStandards
+                .Where(v => v.FromDate <= this.Session().Now() && (!v.ExistThroughDate || v.ThroughDate >= this.Session().Now()))
+                .ToArray();
+
+            this.InactiveWorkEffortPartStandards = this.WorkEffortPartStandards
+                .Except(this.CurrentWorkEffortPartStandards)
+                .ToArray();
         }
     }
 }
