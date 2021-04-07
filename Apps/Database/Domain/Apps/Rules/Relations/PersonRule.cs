0c99a2cf2e54f1390d30eb9073e5306331334679
Joren Schelkens <52953553+JorenSchelkens@users.noreply.github.com>
2021-04-07 14:26:26 +0200

    Added Rules

--- Apps/Database/Domain/Apps/Rules/Relations/PersonRule.cs
+++ Apps/Database/Domain/Apps/Rules/Relations/PersonRule.cs
@@ -18,87 +18,18 @@ namespace Allors.Database.Domain
             this.Patterns = new Pattern[]
             {
                 new RolePattern(m.Person, m.Person.DerivationTrigger),
-                new RolePattern(m.Person, m.Person.Salutation),
-                new RolePattern(m.Person, m.Person.FirstName),
-                new RolePattern(m.Person, m.Person.MiddleName),
-                new RolePattern(m.Person, m.Person.LastName),
-                new RolePattern(m.Person, m.Person.UserName),
-                new AssociationPattern(m.OrganisationContactRelationship.Contact),
                 new RolePattern(m.OrganisationContactRelationship, m.OrganisationContactRelationship.FromDate) {Steps = new IPropertyType[]{ m.OrganisationContactRelationship.Contact } },
                 new RolePattern(m.OrganisationContactRelationship, m.OrganisationContactRelationship.ThroughDate) {Steps = new IPropertyType[]{ m.OrganisationContactRelationship.Contact } },
-                new RolePattern(m.Employment, m.Employment.FromDate) { Steps =  new IPropertyType[] {m.Employment.Employee} },
-                new RolePattern(m.Employment, m.Employment.ThroughDate) { Steps =  new IPropertyType[] {m.Employment.Employee} },
             };
 
         public override void Derive(IDomainDerivationCycle cycle, IEnumerable<IObject> matches)
         {
             foreach (var @this in matches.Cast<Person>())
             {
-                var now = @this.Transaction().Now();
-
                 @this.Strategy.Transaction.Prefetch(@this.PrefetchPolicy);
 
-                if (@this.ExistSalutation
-                    && (@this.Salutation.Equals(new Salutations(@this.Transaction()).Mr)
-                        || @this.Salutation.Equals(new Salutations(@this.Transaction()).Dr)))
-                {
-                    @this.Gender = new GenderTypes(@this.Transaction()).Male;
-                }
-
-                if (@this.ExistSalutation
-                    && (@this.Salutation.Equals(new Salutations(@this.Transaction()).Mrs)
-                        || @this.Salutation.Equals(new Salutations(@this.Transaction()).Ms)
-                        || @this.Salutation.Equals(new Salutations(@this.Transaction()).Mme)))
-                {
-                    @this.Gender = new GenderTypes(@this.Transaction()).Female;
-                }
-
-                @this.PartyName = DerivePartyName(@this);
-
                 @this.DeriveRelationships();
-
-                if (!@this.ExistTimeSheetWhereWorker && (@this.AppsIsActiveEmployee(now) || @this.CurrentOrganisationContactRelationships.Count > 0))
-                {
-                    new TimeSheetBuilder(@this.Strategy.Transaction).WithWorker(@this).Build();
-                }
             }
-        }
-
-        static string DerivePartyName(Person person)
-        {
-            var partyName = new StringBuilder();
-
-            if (person.ExistFirstName)
-            {
-                partyName.Append(person.FirstName);
-            }
-
-            if (person.ExistMiddleName)
-            {
-                if (partyName.Length > 0)
-                {
-                    partyName.Append(" ");
-                }
-
-                partyName.Append(person.MiddleName);
-            }
-
-            if (person.ExistLastName)
-            {
-                if (partyName.Length > 0)
-                {
-                    partyName.Append(" ");
-                }
-
-                partyName.Append(person.LastName);
-            }
-
-            if (partyName.Length == 0)
-            {
-                partyName.Append($"[{person.UserName}]");
-            }
-
-            return partyName.ToString();
         }
     }
 }
