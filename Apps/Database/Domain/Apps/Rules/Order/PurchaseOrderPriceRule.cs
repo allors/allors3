f5771b0adacc93e98346b486827d61e6767bd181
martienvanknippenberg <martien@dipu.com>
2021-05-17 10:03:20 +0200

    PurchaseOrderPriceRule

--- Apps/Database/Domain/Apps/Rules/Order/PurchaseOrderPriceRule.cs
+++ Apps/Database/Domain/Apps/Rules/Order/PurchaseOrderPriceRule.cs
@@ -97,6 +97,9 @@ namespace Allors.Database.Domain
                 @this.TotalExtraCharge = 0;
                 @this.TotalIncVat = 0;
                 @this.GrandTotal = 0;
+                @this.TotalShippingAndHandlingInPreferredCurrency = 0;
+                @this.TotalFeeInPreferredCurrency = 0;
+                @this.TotalExtraChargeInPreferredCurrency = 0;
 
                 foreach (PurchaseOrderItem orderItem in @this.ValidOrderItems)
                 {
