718f7003417e7908590156f2d67b0b2404016d2e
Koen Van Exem <koen@vanexem.be>
2021-06-17 11:36:41 +0200

    update nx

--- typescript/libs/workspace/meta/core-generated/jest.config.js
+++ typescript/libs/workspace/meta/core-generated/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'workspace-domain-core-tests',
+  displayName: 'workspace-meta-core-generated',
   preset: '../../../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -10,5 +10,5 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../../coverage/libs/workspace/domain/core-tests',
+  coverageDirectory: '../../../../coverage/libs/workspace/meta/core-generated',
 };
