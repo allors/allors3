ff99dd4780866b42fc77aa6db070204d7e24874d
Lucca Boets <boetslucca@gmail.com>
2021-07-15 12:02:45 +0200

    Update AssertExtensions.cs

--- dotnet/Core/Workspace/CSharp/tests/extensions/AssertExtensions.cs
+++ dotnet/Core/Workspace/CSharp/tests/extensions/AssertExtensions.cs
@@ -8,6 +8,7 @@ namespace Tests.Workspace
     {
         public static void ShouldEqual(this object actual, object expected, Context context, Mode mode) => Assert.True(Equals(actual, expected), $"Expected Equals: [{actual}, {expected}] on context {context} with mode {mode}");
         public static void ShouldNotBeNull(this object actual, Context context, Mode mode) => Assert.True(!(actual is null), $"Expected Not Null: [{actual}] on context {context} with mode {mode}");
-        public static void ShouldContains(this IEnumerable<object> colletion, object expected, Context context, Mode mode) => Assert.True(colletion.Contains(expected), $"Expected Not Null: [{colletion}, {expected}] on context {context} with mode {mode}");
+        public static void ShouldContains(this IEnumerable<object> colletion, object expected, Context context, Mode mode) => Assert.True(colletion.Contains(expected), $"Expected Contains: [{colletion}, {expected}] on context {context} with mode {mode}");
+        public static void ShouldNotContains(this IEnumerable<object> colletion, object expected, Context context, Mode mode) => Assert.True(!colletion.Contains(expected), $"Expected Not Contains: [{colletion}, {expected}] on context {context} with mode {mode}");
     }
 }
