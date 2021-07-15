219b62520fa23747880aecfbccbc286d500d595f
Lucca Boets <boetslucca@gmail.com>
2021-07-15 12:00:06 +0200

    Update AssertExtensions.cs

--- dotnet/Core/Workspace/CSharp/tests/extensions/AssertExtensions.cs
+++ dotnet/Core/Workspace/CSharp/tests/extensions/AssertExtensions.cs
@@ -1,10 +1,13 @@
 namespace Tests.Workspace
 {
+    using System.Collections.Generic;
+    using System.Linq;
     using Xunit;
 
     public static class AssertExtensions
     {
         public static void ShouldEqual(this object actual, object expected, Context context, Mode mode) => Assert.True(Equals(actual, expected), $"Expected Equals: [{actual}, {expected}] on context {context} with mode {mode}");
         public static void ShouldNotBeNull(this object actual, Context context, Mode mode) => Assert.True(!(actual is null), $"Expected Not Null: [{actual}] on context {context} with mode {mode}");
+        public static void ShouldContains(this IEnumerable<object> colletion, object expected, Context context, Mode mode) => Assert.True(colletion.Contains(expected), $"Expected Not Null: [{colletion}, {expected}] on context {context} with mode {mode}");
     }
 }
