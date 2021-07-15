57efcb2fbbaf090d218438d09e8bf25e3b7e0b93
Joren Schelkens <52953553+JorenSchelkens@users.noreply.github.com>
2021-07-15 13:30:24 +0200

    Changed AssertExtensions

--- dotnet/Core/Workspace/CSharp/tests/tests/SandboxTests.cs
+++ dotnet/Core/Workspace/CSharp/tests/tests/SandboxTests.cs
@@ -5,16 +5,18 @@
 
 namespace Tests.Workspace
 {
+    using System;
     using System.Linq;
     using System.Threading.Tasks;
     using Allors.Workspace;
     using Allors.Workspace.Data;
     using Allors.Workspace.Domain;
-    using Antlr.Runtime.Misc;
     using Xunit;
 
     public abstract class SandboxTests : Test
     {
+        private Func<ISession, Task>[] pushes;
+
         private Func<Context>[] contextFactories;
 
         protected SandboxTests(Fixture fixture) : base(fixture)
@@ -25,6 +27,12 @@ namespace Tests.Workspace
         {
             await base.InitializeAsync();
             await this.Login("administrator");
+
+            this.pushes = new Func<ISession, Task>[]
+            {
+                (session) => Task.CompletedTask,
+                async (session) => await session.Push()
+            };
 
             var singleSessionContext = new SingleSessionContext(this, "Single shared");
             var multipleSessionContext = new MultipleSessionContext(this, "Multiple shared");
@@ -41,34 +49,43 @@ namespace Tests.Workspace
         [Fact]
         public async void Test()
         {
-            foreach (Mode mode in System.Enum.GetValues(typeof(Mode)))
+            foreach (var push1 in this.pushes)
             {
-                foreach (var contextFactory in this.contextFactories)
+                foreach (var push2 in this.pushes)
                 {
-                    var ctx = contextFactory();
-                    var (session1, session2) = ctx;
-
-                    var c1x_1 = await ctx.Create<C1>(session1, mode);
-                    var c1y_2 = await ctx.Create<C1>(session2, mode);
-
-                    c1x_1.ShouldNotBeNull(ctx, mode);
-                    c1y_2.ShouldNotBeNull(ctx, mode);
-
-                    await session2.Push();
-                    var result = await session1.Pull(new Pull { Object = c1y_2 });
-
-                    var c1y_1 = (C1)result.Objects.Values.First();
-
-                    c1y_1.ShouldNotBeNull(ctx, mode);
-
-                    if (!c1x_1.CanWriteC1C1One2One)
+                    foreach (Mode mode1 in Enum.GetValues(typeof(Mode)))
                     {
-                        await session1.Pull(new Pull { Object = c1x_1 });
+                        foreach (Mode mode2 in Enum.GetValues(typeof(Mode)))
+                        {
+                            foreach (var contextFactory in this.contextFactories)
+                            {
+                                var ctx = contextFactory();
+                                var (session1, session2) = ctx;
+
+                                var c1x_1 = await ctx.Create<C1>(session1, mode1);
+                                var c1y_2 = await ctx.Create<C1>(session2, mode1);
+
+                                c1x_1.ShouldNotBeNull(ctx, mode1, mode2);
+                                c1y_2.ShouldNotBeNull(ctx, mode1, mode2);
+
+                                await session2.Push();
+                                var result = await session1.Pull(new Pull { Object = c1y_2 });
+
+                                var c1y_1 = (C1)result.Objects.Values.First();
+
+                                c1y_1.ShouldNotBeNull(ctx, mode1, mode2);
+
+                                if (!c1x_1.CanWriteC1C1One2One)
+                                {
+                                    await session1.Pull(new Pull { Object = c1x_1 });
+                                }
+
+                                c1x_1.C1C1One2One = c1y_1;
+
+                                c1y_1.ShouldEqual(c1x_1.C1C1One2One, ctx, mode1, mode2);
+                            }
+                        }
                     }
-
-                    c1x_1.C1C1One2One = c1y_1;
-
-                    c1y_1.ShouldEqual(c1x_1.C1C1One2One, ctx, mode);
                 }
             }
         }
--- dotnet/Core/Workspace/CSharp/tests/tests/session/database/ManyToManyTests.cs
+++ dotnet/Core/Workspace/CSharp/tests/tests/session/database/ManyToManyTests.cs
@@ -9,12 +9,15 @@ namespace Tests.Workspace.SessionDatabase
     using Allors.Workspace.Domain;
     using Allors.Workspace;
     using Xunit;
+    using System;
+    using Allors.Workspace.Data;
+    using System.Linq;
 
     public abstract class ManyToManyTests : Test
     {
-        private ISession session1;
-        private SessionC1 c1;
-        private C2 c2;
+        private Func<ISession, Task>[] pushes;
+
+        private Func<Context>[] contextFactories;
 
         protected ManyToManyTests(Fixture fixture) : base(fixture)
         {
@@ -24,69 +27,118 @@ namespace Tests.Workspace.SessionDatabase
         public override async Task InitializeAsync()
         {
             await base.InitializeAsync();
-
             await this.Login("administrator");
 
-            this.session1 = this.Workspace.CreateSession();
+            this.pushes = new Func<ISession, Task>[]
+            {
+                (session) => Task.CompletedTask,
+                async (session) => await session.Push()
+            };
 
-            this.c1 = this.session1.Create<SessionC1>();
-            this.c2 = this.session1.Create<C2>();
+            var singleSessionContext = new SingleSessionContext(this, "Single shared");
+            var multipleSessionContext = new MultipleSessionContext(this, "Multiple shared");
+
+            this.contextFactories = new Func<Context>[]
+            {
+                () => singleSessionContext,
+                () => new SingleSessionContext(this, "Single"),
+                () => multipleSessionContext,
+                () => new MultipleSessionContext(this, "Multiple"),
+            };
         }
 
         [Fact]
-        public void SetRole_WithoutPush()
+        public async void SetRole()
         {
-            this.c1.AddSessionC1DatabaseC2Many2Many(this.c2);
+            foreach (var push1 in this.pushes)
+            {
+                foreach (Mode mode in Enum.GetValues(typeof(Mode)))
+                {
+                    foreach (var contextFactory in this.contextFactories)
+                    {
+                        var ctx = contextFactory();
+                        var (session1, session2) = ctx;
 
-            Assert.Contains(this.c2, this.c1.SessionC1DatabaseC2Many2Manies);
+                        var c1x_1 = ctx.Session1.Create<SessionC1>();
+                        var c1y_2 = await ctx.Create<C1>(session2, mode);
+
+                        c1x_1.ShouldNotBeNull(ctx, mode);
+                        c1y_2.ShouldNotBeNull(ctx, mode);
+
+                        await session2.Push();
+
+                        var result = await session1.Pull(new Pull { Object = c1y_2 });
+
+                        var c1y_1 = (C1)result.Objects.Values.First();
+
+                        c1y_1.ShouldNotBeNull(ctx, mode);
+
+                        c1x_1.AddSessionC1DatabaseC1Many2Many(c1y_1);
+
+                        c1x_1.SessionC1DatabaseC1Many2Manies.ShouldContains(c1y_1, ctx, mode);
+
+                        await push1(session1);
+
+                        c1x_1.SessionC1DatabaseC1Many2Manies.ShouldContains(c1y_1, ctx, mode);
+                    }
+                }
+            }
         }
 
-        [Fact]
-        public async void SetRole_WithPush()
-        {
-            await this.session1.Push();
+        //[Fact]
+        //public void SetRole_WithoutPush()
+        //{
+        //    this.c1.AddSessionC1DatabaseC2Many2Many(this.c2);
 
-            this.c1.AddSessionC1DatabaseC2Many2Many(this.c2);
+        //    Assert.Contains(this.c2, this.c1.SessionC1DatabaseC2Many2Manies);
+        //}
 
-            Assert.Contains(this.c2, this.c1.SessionC1DatabaseC2Many2Manies);
+        //[Fact]
+        //public async void SetRole_WithPush()
+        //{
+        //    await this.session1.Push();
 
-            await this.session1.Push();
+        //    this.c1.AddSessionC1DatabaseC2Many2Many(this.c2);
 
-            Assert.Contains(this.c2, this.c1.SessionC1DatabaseC2Many2Manies);
-        }
+        //    Assert.Contains(this.c2, this.c1.SessionC1DatabaseC2Many2Manies);
 
-        [Fact]
-        public void RemoveRole_WithoutPush()
-        {
-            this.c1.AddSessionC1DatabaseC2Many2Many(this.c2);
+        //    await this.session1.Push();
 
-            Assert.Contains(this.c2, this.c1.SessionC1DatabaseC2Many2Manies);
+        //    Assert.Contains(this.c2, this.c1.SessionC1DatabaseC2Many2Manies);
+        //}
 
-            this.c1.RemoveSessionC1DatabaseC2Many2Many(this.c2);
+        //[Fact]
+        //public void RemoveRole_WithoutPush()
+        //{
+        //    this.c1.AddSessionC1DatabaseC2Many2Many(this.c2);
 
-            Assert.DoesNotContain(this.c2, this.c1.SessionC1DatabaseC2Many2Manies);
-        }
+        //    Assert.Contains(this.c2, this.c1.SessionC1DatabaseC2Many2Manies);
 
-        [Fact]
-        public async void RemoveRole_WithPush()
-        {
-            await this.session1.Push();
+        //    this.c1.RemoveSessionC1DatabaseC2Many2Many(this.c2);
 
-            this.c1.AddSessionC1DatabaseC2Many2Many(this.c2);
+        //    Assert.DoesNotContain(this.c2, this.c1.SessionC1DatabaseC2Many2Manies);
+        //}
 
-            Assert.Contains(this.c2, this.c1.SessionC1DatabaseC2Many2Manies);
+        //[Fact]
+        //public async void RemoveRole_WithPush()
+        //{
+        //    await this.session1.Push();
 
-            await this.session1.Push();
+        //    this.c1.AddSessionC1DatabaseC2Many2Many(this.c2);
 
-            Assert.Contains(this.c2, this.c1.SessionC1DatabaseC2Many2Manies);
+        //    Assert.Contains(this.c2, this.c1.SessionC1DatabaseC2Many2Manies);
 
-            this.c1.RemoveSessionC1DatabaseC2Many2Many(this.c2);
+        //    await this.session1.Push();
 
-            Assert.DoesNotContain(this.c2, this.c1.SessionC1DatabaseC2Many2Manies);
+        //    Assert.Contains(this.c2, this.c1.SessionC1DatabaseC2Many2Manies);
 
-            await this.session1.Push();
+        //    this.c1.RemoveSessionC1DatabaseC2Many2Many(this.c2);
 
-            Assert.DoesNotContain(this.c2, this.c1.SessionC1DatabaseC2Many2Manies);
-        }
+        //    Assert.DoesNotContain(this.c2, this.c1.SessionC1DatabaseC2Many2Manies);
+
+        //    await this.session1.Push();
+
+        //    Assert.DoesNotContain(this.c2, this.c1.SessionC1DatabaseC2Many2Manies);
+        //}
     }
 }
