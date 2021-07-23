f10b3a3adec4e649729287948b2ce4b3a3d3a4e8
Koen Van Exem <koen@vanexem.be>
2021-07-23 14:52:26 +0200

    Blazor (WIP)

--- dotnet/Core/workspace/csharp/blazors.bootstrap.server/Startup.cs
+++ dotnet/Core/workspace/csharp/blazors.bootstrap.server/Startup.cs
@@ -1,12 +1,17 @@
 namespace Blazors.Bootstrap.Server
 {
+    using System.Linq;
     using Allors.Database.Adapters;
     using Allors.Database.Configuration;
     using Allors.Database.Domain;
     using Allors.Database.Domain.Derivations.Rules.Default;
     using Allors.Database.Meta;
+    using Allors.Ranges;
     using Allors.Security;
     using Allors.Services;
+    using Allors.Workspace;
+    using Allors.Workspace.Adapters;
+    using Allors.Workspace.Meta;
     using Blazors.Bootstrap.Server.Areas.Identity;
     using BlazorStrap;
     using Microsoft.AspNetCore.Builder;
@@ -14,9 +19,10 @@ namespace Blazors.Bootstrap.Server
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Identity;
-    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
+    using Data = Allors.Database.Domain.Data;
+    using IConfiguration = Microsoft.Extensions.Configuration.IConfiguration;
     using ObjectFactory = Allors.Database.ObjectFactory;
     using User = Allors.Database.Meta.User;
 
@@ -47,6 +53,33 @@ namespace Blazors.Bootstrap.Server
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
+
+            var workspaceMetaPopulation = new Allors.Workspace.Meta.Lazy.MetaBuilder().Build();
+            var reflectionObjectFactory = new ReflectionObjectFactory(workspaceMetaPopulation, typeof(Allors.Workspace.Domain.Person));
+            var workspaceConfiguration = new Allors.Workspace.Adapters.Local.Configuration(
+                "Default",
+                workspaceMetaPopulation,
+                reflectionObjectFactory);
+
+            services.AddScoped<IWorkspace>((serviceProvider) =>
+            {
+                var databaseService = serviceProvider.GetRequiredService<IDatabaseService>();
+                var database = databaseService.Database;
+
+                var httpContextAccessor = serviceProvider.GetRequiredService<IHttpContextAccessor>();
+                var identity = httpContextAccessor.HttpContext.User.Identity.Name;
+                var user = new People(database.CreateTransaction())
+                    .Extent()
+                    .First(v => identity.ToUpperInvariant().Equals(v.NormalizedUserName));
+
+                var databaseConnection = new Allors.Workspace.Adapters.Local.DatabaseConnection(workspaceConfiguration,
+                    database, () => new WorkspaceContext(), () => new DefaultRanges())
+                {
+                    UserId = user.Strategy.ObjectId
+                };
+
+                return databaseConnection.CreateWorkspace();
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IHttpContextAccessor httpContextAccessor)
@@ -58,7 +91,6 @@ namespace Blazors.Bootstrap.Server
             var databaseScope = new DefaultDomainDatabaseServices(engine, httpContextAccessor);
             var databaseBuilder = new DatabaseBuilder(databaseScope, this.Configuration, objectFactory);
             app.ApplicationServices.GetRequiredService<IDatabaseService>().Database = databaseBuilder.Build();
-
 
             if (env.IsDevelopment())
             {
