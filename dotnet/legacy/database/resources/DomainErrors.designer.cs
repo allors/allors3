164bf9c5647a0fb18969e32c924cd0ac25111579
martienvanknippenberg <martien@dipu.com>
2021-11-29 16:12:57 +0100

    error messages

--- dotnet/legacy/database/resources/DomainErrors.designer.cs
+++ dotnet/legacy/database/resources/DomainErrors.designer.cs
@@ -22,14 +22,14 @@ namespace Resources {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class _domainerrors {
+    public class DomainErrors {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal _domainerrors() {
+        internal DomainErrors() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace Resources {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Resources._domainerrors", typeof(_domainerrors).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Resources.DomainErrors", typeof(DomainErrors).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
