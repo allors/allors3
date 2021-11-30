0bc46ebc5decc0e1a15dae84cf5039bf31d39bb9
Koen Van Exem <koen@vanexem.be>
2021-11-30 23:41:00 +0100

    Fix build

--- dotnet/Base/repository/domain/custom/database/singleton.cs
+++ dotnet/Base/repository/domain/custom/database/singleton.cs
@@ -8,63 +8,14 @@ namespace Allors.Repository
     using Attributes;
     using static Workspaces;
 
-    #region Allors
-    [Id("313b97a5-328c-4600-9dd2-b5bc146fb13b")]
-    #endregion
     public partial class Singleton : Object
     {
-        #region inherited properties
-        public Revocation[] Revocations { get; set; }
-
-        public SecurityToken[] SecurityTokens { get; set; }
-
-        #endregion
-
         #region Allors
-        [Id("9c1634ab-be99-4504-8690-ed4b39fec5bc")]
+        [Id("AD981D2E-32E8-4DC6-91A2-F8A2F44086F3")]
         #endregion
         [Multiplicity(Multiplicity.ManyToOne)]
         [Workspace(Default)]
         [Indexed]
-        public Locale DefaultLocale { get; set; }
-
-        #region Allors
-        [Id("9e5a3413-ed33-474f-adf2-149ad5a80719")]
-        #endregion
-        [Multiplicity(Multiplicity.OneToMany)]
-        [Indexed]
-        [Workspace(Default)]
-        public Locale[] AdditionalLocales { get; set; }
-
-        #region Allors
-        [Id("615AC72B-B3DF-4057-9B7C-C8528341F5FE")]
-        #endregion
-        [Multiplicity(Multiplicity.OneToMany)]
-        [Indexed]
-        [Derived]
-        [Workspace(Default)]
-        public Locale[] Locales { get; set; }
-
-        #region Allors
-        [Id("B2166062-84DA-449D-B34F-983A0C81BC31")]
-        #endregion
-        [Multiplicity(Multiplicity.ManyToOne)]
-        [Indexed]
-        [Workspace(Default)]
-        public Media LogoImage { get; set; }
-
-        #region inherited methods
-
-        public void OnBuild() { }
-
-        public void OnPostBuild() { }
-
-        public void OnInit()
-        {
-        }
-
-        public void OnPostDerive() { }
-
-        #endregion
+        public Person AutocompleteDefault { get; set; }
     }
 }
