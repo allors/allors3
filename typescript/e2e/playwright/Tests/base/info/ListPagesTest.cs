758df3a46063cf37ed47dd614bca5599684b5e2a
Koen Van Exem <koen@vanexem.be>
2021-12-15 18:11:08 +0100

    Playwright (WIP)

--- typescript/e2e/playwright/Tests/base/info/ListPagesTest.cs
+++ typescript/e2e/playwright/Tests/base/info/ListPagesTest.cs
@@ -5,8 +5,6 @@
 
 namespace Tests.ApplicationTests
 {
-    using System;
-    using System.ComponentModel;
     using System.Linq;
     using Allors.E2E.Angular;
     using Allors.E2E.Angular.Info;
@@ -71,10 +69,11 @@ namespace Tests.ApplicationTests
                 {
                     foreach (var @object in this.Transaction.Instantiate(await table.GetObjectIds()))
                     {
-                        await table.Action(@object, "edit");
                         await this.Page.WaitForAngular();
+                        await table.Action(@object, "edit");
 
-
+                        await this.Page.WaitForAngular();
+                        await this.Page.Keyboard.PressAsync("Escape");
                     }
                 }
             }
@@ -82,43 +81,30 @@ namespace Tests.ApplicationTests
             Assert.IsEmpty(this.ConsoleErrorMessages);
         }
 
-        //[Test]
-        //public async Task Overview()
-        //{
-        //    this.Login();
+        [Test]
+        public async Task Overview()
+        {
+            foreach (var component in this.Components.Where(v => v.List != null))
+            {
+                await this.Page.GotoAsync(component.RouteInfo.FullPath);
+                await this.Page.WaitForAngular();
 
-        //    foreach (var navigateTo in this.navigateTos)
-        //    {
-        //        var page = (Component)navigateTo.Invoke(this.Sidenav, null);
+                var table = new AllorsMaterialTableComponent(this.AppRoot);
+                var actions = await table.Actions();
+                if (actions.Contains("overview"))
+                {
+                    foreach (var @object in this.Transaction.Instantiate(await table.GetObjectIds()))
+                    {
+                        await this.Page.WaitForAngular();
+                        await table.Action(@object, "overview");
 
-        //        var tableProperty = page.GetType().GetProperties().FirstOrDefault(v => v.PropertyType == typeof(MatTable));
-        //        if (tableProperty != null)
-        //        {
-        //            var table = (MatTable)tableProperty?.GetGetMethod().Invoke(page, null);
+                        await this.Page.WaitForAngular();
+                        await this.Page.GoBackAsync();
+                    }
+                }
+            }
 
-        //            if (this.Driver.SelectorIsVisible(table.Selector))
-        //            {
-        //                var action = table.Actions.FirstOrDefault(v => v.Equals("overview"));
-        //                if (action != null)
-        //                {
-        //                    var objects = this.Transaction.Instantiate(table.ObjectIds);
-        //                    foreach (var @object in objects)
-        //                    {
-        //                        table.Action(@object, action);
-        //                        this.Driver.Navigate().Back();
-        //                    }
-        //                }
-        //            }
-        //        }
-        //    }
-        //}
-
-        //private static void Cancel(IComponent dialog)
-        //{
-        //    var cancelProperty = dialog?.GetType().GetProperties().FirstOrDefault(v => v.Name.Equals("cancel", StringComparison.InvariantCultureIgnoreCase));
-        //    dynamic cancel = cancelProperty?.GetGetMethod().Invoke(dialog, null);
-
-        //    cancel?.Click();
-        //}
+            Assert.IsEmpty(this.ConsoleErrorMessages);
+        }
     }
 }
