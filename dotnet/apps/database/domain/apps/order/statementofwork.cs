e3ee5e594431ab4bed9f35716b86ecac5931e792
martienvanknippenberg <martien@dipu.com>
2022-02-07 11:09:00 +0100

    StatementOfWork

--- dotnet/apps/database/domain/apps/order/statementofwork.cs
+++ dotnet/apps/database/domain/apps/order/statementofwork.cs
@@ -3,6 +3,8 @@
 // Licensed under the LGPL license. See LICENSE file in the project root for full license information.
 // </copyright>
 
+using System.Collections.Generic;
+
 namespace Allors.Database.Domain
 {
     public partial class StatementOfWork
@@ -18,6 +20,35 @@ namespace Allors.Database.Domain
             {
                 deletable.Strategy.Delete();
             }
+        }
+
+        public void AppsPrint(PrintablePrint method)
+        {
+            var singleton = this.Strategy.Transaction.GetSingleton();
+            var logo = this.Issuer?.ExistLogoImage == true ?
+                            this.Issuer.LogoImage.MediaContent.Data :
+                            singleton.LogoImage.MediaContent.Data;
+
+            var images = new Dictionary<string, byte[]>
+                                {
+                                    { "Logo1", logo },
+                                    { "Logo2", logo },
+                                };
+
+            if (this.ExistQuoteNumber)
+            {
+                var transaction = this.Strategy.Transaction;
+                var barcodeGenerator = transaction.Database.Services.Get<IBarcodeGenerator>();
+                var barcode = barcodeGenerator.Generate(this.QuoteNumber, BarcodeType.CODE_128, 320, 80, pure: true);
+                images.Add("Barcode", barcode);
+            }
+
+            var printModel = new Print.StatementOfWorkModel.Model(this, images);
+            this.RenderPrintDocument(this.Issuer?.StatementOfWorkTemplate, printModel, images);
+
+            this.PrintDocument.Media.InFileName = $"{this.QuoteNumber}.odt";
+
+            method.StopPropagation = true;
         }
     }
 }
