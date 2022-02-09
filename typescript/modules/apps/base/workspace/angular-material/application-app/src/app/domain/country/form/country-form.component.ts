b390c3beef61e4aef7956f239e4a5a8318886a8a
KoenVanExem <koen@vanexem.be>
2022-02-09 10:50:59 +0100

    Workspace (WIP)

--- typescript/modules/apps/base/workspace/angular-material/application-app/src/app/domain/country/form/country-form.component.ts
+++ typescript/modules/apps/base/workspace/angular-material/application-app/src/app/domain/country/form/country-form.component.ts
@@ -6,17 +6,21 @@ import {
 } from '@allors/base/workspace/angular/foundation';
 import { ContextService } from '@allors/base/workspace/angular/foundation';
 import { NgForm } from '@angular/forms';
+import { M } from '@allors/default/workspace/meta';
 
 @Component({
   templateUrl: 'country-form.component.html',
   providers: [ContextService],
 })
 export class CountryFormComponent extends AllorsFormComponent<Country> {
+  m: M;
+
   constructor(
     @Self() allors: ContextService,
     errorService: ErrorService,
     form: NgForm
   ) {
     super(allors, errorService, form);
+    this.m = allors.metaPopulation as M;
   }
 }
