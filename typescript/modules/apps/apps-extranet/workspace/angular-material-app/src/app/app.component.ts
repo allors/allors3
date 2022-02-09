fc52a20669bf343abaa89391270d01bf926d65ae
KoenVanExem <koen@vanexem.be>
2022-02-09 10:16:28 +0100

    Workspace (WIP)

--- typescript/modules/apps/apps-extranet/workspace/angular-material-app/src/app/app.component.ts
+++ typescript/modules/apps/apps-extranet/workspace/angular-material-app/src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'allors-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-apps-app';
--- typescript/modules/libs/apps-extranet/workspace/angular-material/src/index.ts
+++ typescript/modules/libs/apps-extranet/workspace/angular-material/src/index.ts
@@ -1,9 +1,9 @@
 export { PrintAction } from './lib/actions/print/print-action';
 export { PrintService, PrintConfig } from './lib/actions/print/print.service';
 
-export { WorkEffortListPageComponent } from './lib/objects/workeffort/list/workeffort-list-page.component';
+export { WorkEffortListPageComponent } from './lib/domain/workeffort/list/workeffort-list-page.component';
 
-export { WorkTaskCreateFormComponent } from './lib/objects/worktask/create/worktask-create-form.component';
-export { WorkTaskOverviewComponent } from './lib/objects/worktask/overview/worktask-overview-page.component';
-export { WorkTaskEditFormComponent } from './lib/objects/worktask/edit/worktask-edit-form.component';
-export { WorkTaskSummaryPanel } from './lib/objects/worktask/summary/worktask-summary-panel.component';
+export { WorkTaskCreateFormComponent } from './lib/domain/worktask/create/worktask-create-form.component';
+export { WorkTaskOverviewComponent } from './lib/domain/worktask/overview/worktask-overview-page.component';
+export { WorkTaskEditFormComponent } from './lib/domain/worktask/edit/worktask-edit-form.component';
+export { WorkTaskSummaryPanel } from './lib/domain/worktask/summary/worktask-summary-panel.component';
