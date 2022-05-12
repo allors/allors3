e0fcc94094aab0e3e71d5690e81a784b8c80adaa
Martien van Knippenberg <martien@dipu.com>
2022-05-12 10:31:47 +0200

    WorkTaskActualHoursRule

--- dotnet/apps/database/domain/apps/rules/workeffort/worktaskactualhoursrule.cs
+++ dotnet/apps/database/domain/apps/rules/workeffort/worktaskactualhoursrule.cs
@@ -31,31 +31,43 @@ namespace Allors.Database.Domain
 
             foreach (var @this in matches.Cast<WorkTask>())
             {
-                @this.ActualHours = 0M;
+                @this.DeriveWorkTaskActualHours(validation);
+            }
+        }
+    }
 
-                foreach (var serviceEntry in @this.ServiceEntriesWhereWorkEffort)
+    public static class WorkTaskActualHoursRuleExtensions
+    {
+        public static void DeriveWorkTaskActualHours(this WorkTask @this, IValidation validation)
+        {
+            @this.ActualHours = 0M;
+
+            foreach (var serviceEntry in @this.ServiceEntriesWhereWorkEffort)
+            {
+                if (serviceEntry is TimeEntry timeEntry)
                 {
-                    if (serviceEntry is TimeEntry timeEntry)
+                    @this.ActualHours += timeEntry.ActualHours;
+
+                    if (!@this.ExistActualStart)
                     {
-                        @this.ActualHours += timeEntry.ActualHours;
+                        @this.ActualStart = timeEntry.FromDate;
+                    }
+                    else if (timeEntry.FromDate < @this.ActualStart)
+                    {
+                        @this.ActualStart = timeEntry.FromDate;
+                    }
 
-                        if (!@this.ExistActualStart)
-                        {
-                            @this.ActualStart = timeEntry.FromDate;
-                        }
-                        else if (timeEntry.FromDate < @this.ActualStart)
-                        {
-                            @this.ActualStart = timeEntry.FromDate;
-                        }
-
-                        if (!@this.ExistActualCompletion)
-                        {
-                            @this.ActualCompletion = timeEntry.ThroughDate;
-                        }
-                        else if (timeEntry.ThroughDate > @this.ActualCompletion)
-                        {
-                            @this.ActualCompletion = timeEntry.ThroughDate;
-                        }
+                    if (!@this.ExistActualCompletion)
+                    {
+                        @this.ActualCompletion = timeEntry.ThroughDate;
+                    }
+                    else if (timeEntry.ThroughDate > @this.ActualCompletion)
+                    {
+                        @this.ActualCompletion = timeEntry.ThroughDate;
+                    }
+                    else if (!timeEntry.ExistThroughDate)
+                    {
+                        @this.RemoveActualCompletion();
                     }
                 }
             }
