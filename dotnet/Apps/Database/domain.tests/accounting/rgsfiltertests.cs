3a834044d335cb2a5266fdcd35945304f752cbfe
Joren Schelkens <joren.schelkens@gmail.com>
2022-07-12 12:48:12 +0200

    Create rgsfiltertests.cs

---
+++ dotnet/Apps/Database/domain.tests/accounting/rgsfiltertests.cs
@@ -0,0 +1,50 @@
+// <copyright file="JournalTests.cs" company="Allors bvba">
+// Copyright (c) Allors bvba. All rights reserved.
+// Licensed under the LGPL license. See LICENSE file in the project root for full license information.
+// </copyright>
+// <summary>Defines the MediaTests type.</summary>
+
+namespace Allors.Database.Domain.Tests
+{
+    using System.Linq;
+    using Resources;
+    using Xunit;
+
+    public class RgsFilterTests : DomainTest, IClassFixture<Fixture>
+    {
+        public RgsFilterTests(Fixture fixture) : base(fixture) { }
+    }
+
+    public class RgsFilterRuleTests : DomainTest, IClassFixture<Fixture>
+    {
+        public RgsFilterRuleTests(Fixture fixture) : base(fixture) { }
+
+        [Fact]
+        public void RgsFilterUseEzAndExcludeEzCanNotBeBothTrue()
+        {
+            var rgsFilter = new RgsFilterBuilder(this.Transaction).Build();
+            var errors = this.Derive().Errors.ToList();
+            Assert.Empty(errors);
+
+            rgsFilter.UseEz = true;
+            rgsFilter.ExcludeEz = true;
+
+            errors = this.Derive().Errors.ToList();
+            Assert.Single(errors);
+        }
+
+        [Fact]
+        public void RgsFilterUseZzpAndExcludeZzpCanNotBeBothTrue()
+        {
+            var rgsFilter = new RgsFilterBuilder(this.Transaction).Build();
+            var errors = this.Derive().Errors.ToList();
+            Assert.Empty(errors);
+
+            rgsFilter.UseZzp = true;
+            rgsFilter.ExcludeZzp = true;
+
+            errors = this.Derive().Errors.ToList();
+            Assert.Single(errors);
+        }
+    }
+}
