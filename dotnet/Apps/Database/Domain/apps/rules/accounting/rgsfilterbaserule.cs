b09a487e5a9061a91445de4b4ec110082923fca1
Lucca Boets <boetslucca@gmail.com>
2022-07-12 12:35:27 +0200

    added rgs filter rules

---
+++ dotnet/Apps/Database/Domain/apps/rules/accounting/rgsfilterbaserule.cs
@@ -0,0 +1,38 @@
+// <copyright file="Domain.cs" company="Allors bvba">
+// Copyright (c) Allors bvba. All rights reserved.
+// Licensed under the LGPL license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace Allors.Database.Domain
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Database.Derivations;
+    using Meta;
+    using Derivations.Rules;
+    using Resources;
+
+    public class RgsFilterBaseRule : Rule
+    {
+        public RgsFilterBaseRule(MetaPopulation m) : base(m, new Guid("4897952b-b01e-4044-8563-00d98a0ff611")) =>
+            this.Patterns = new Pattern[]
+            {
+                m.RgsFilter.RolePattern(v => v.UseBase),
+                m.RgsFilter.RolePattern(v => v.UseExtended)
+            };
+
+        public override void Derive(ICycle cycle, IEnumerable<IObject> matches)
+        {
+            var validation = cycle.Validation;
+
+            foreach (var @this in matches.Cast<RgsFilter>())
+            {
+                if (@this.UseBase && @this.UseExtended)
+                {
+                    validation.AddError(@this, @this.Meta.UseBase, "UseBase and UseExtended can't be both True");
+                }
+            }
+        }
+    }
+}
---
+++ dotnet/Apps/Database/Domain/apps/rules/accounting/rgsfilterwocorule.cs
@@ -0,0 +1,38 @@
+// <copyright file="Domain.cs" company="Allors bvba">
+// Copyright (c) Allors bvba. All rights reserved.
+// Licensed under the LGPL license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace Allors.Database.Domain
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Database.Derivations;
+    using Meta;
+    using Derivations.Rules;
+    using Resources;
+
+    public class RgsFilterWoCoRule : Rule
+    {
+        public RgsFilterWoCoRule(MetaPopulation m) : base(m, new Guid("f46875ad-2166-4997-ba4a-07e798b6c897")) =>
+            this.Patterns = new Pattern[]
+            {
+                m.RgsFilter.RolePattern(v => v.UseWoCo),
+                m.RgsFilter.RolePattern(v => v.ExcludeWoCo),
+            };
+        
+        public override void Derive(ICycle cycle, IEnumerable<IObject> matches)
+        {
+            var validation = cycle.Validation;
+
+            foreach (var @this in matches.Cast<RgsFilter>())
+            {
+                if (@this.ExcludeWoCo.HasValue && @this.UseWoCo && @this.ExcludeWoCo.Value)
+                {
+                    validation.AddError(@this, @this.Meta.UseBase, "UseWoCo and ExcludeWoCo can't be both True");
+                }
+            }
+        }
+    }
+}
