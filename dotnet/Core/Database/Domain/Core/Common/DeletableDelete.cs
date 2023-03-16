345d486f07f93fbf3371ccf5f032b2ca95989319
DriesLeyers2002 <s120724@ap.be>
2023-03-16 16:45:17 +0100

    Deleting

--- dotnet/Core/Database/Domain/Core/Common/DeletableDelete.cs
+++ dotnet/Core/Database/Domain/Core/Common/DeletableDelete.cs
@@ -5,10 +5,16 @@
 
 namespace Allors.Database.Domain
 {
+    using System.Collections.Generic;
+
     public partial class DeletableDelete
     {
+        public ISet<Deletable> Deleting { get; set; }
+
         public override void Execute()
         {
+            this.Deleting ??= new HashSet<Deletable>();
+            this.Deleting.Add((Deletable)this.Object);
             base.Execute();
 
             this.Object.Strategy.Delete();
