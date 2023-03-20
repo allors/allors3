a7745b9c501764bb72943fb786959f45385594e4
Koen Van Exem <koen@vanexem.be>
2023-03-20 17:42:28 +0100

    Blazor (WIP)

--- dotnet/System/Workspace/Blazor/Allors.Workspace.Blazor.Ant/Forms/Roles/ANumberInput.razor.cs
+++
@@ -1,7 +0,0 @@
-namespace Allors.Workspace.Blazor.Ant.Forms.Roles
-{
-    public class ANumberInputBase : RoleField
-    {
-        public int? IntModel { get => (int?)this.Model; set => this.Model = value; }
-    }
-}
--- dotnet/System/Workspace/Blazor/Allors.Workspace.Blazor.Ant/Forms/Roles/AStringInput.razor.cs
+++
@@ -1,7 +0,0 @@
-namespace Allors.Workspace.Blazor.Ant.Forms.Roles
-{
-    public class AStringInputBase : RoleField
-    {
-        public string StringModel { get => (string)this.Model; set => this.Model = value; }
-    }
-}
