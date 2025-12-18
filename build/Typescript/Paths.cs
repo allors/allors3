926dcf98b9c9aa102eba5292631a25b222b59f0c
Koen Van Exem <koen@vanexem.be>
2025-12-18 09:59:20 +0100

    Add typescript path helpers to build system
    Move path definitions from Modules/Paths.cs to Paths.cs with simplified names.

--- build/Typescript/Paths.cs
+++ build/Typescript/Paths.cs
@@ -4,4 +4,11 @@ public partial class Paths
 {
     public AbsolutePath Typescript => Root / "typescript";
 
+    public AbsolutePath TypescriptApps => Typescript / "apps";
+
+    public AbsolutePath TypescriptBaseAngularMaterial => TypescriptApps / "base/workspace/angular-material";
+
+    public AbsolutePath TypescriptLibs => Typescript / "libs";
+
+    public AbsolutePath TypescriptAppsAppsIntranetAngularMaterial => TypescriptLibs / "apps-intranet/workspace/angular-material";
 }
